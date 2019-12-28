76dc0040171f639c79644ed2e1456ddec23e24ad
Alberto González Balderas <albertoglezb@hotmail.com>
2019-12-28 12:13:59 -0600

    Se agregaron las pruebas a los componentes con los snapshot. Se termino la clase: 'Snapshot'.

--- src/__test__/components/Footer.test.js
+++ src/__test__/components/Footer.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { mount } from 'enzyme';
 import Footer from '../../components/Footer';
-
+import { create } from 'react-test-renderer';
 describe('Test footer', () => {
   const footer = mount(<Footer />);
   test('Render del componente footer', () => {
@@ -9,5 +9,12 @@ describe('Test footer', () => {
   });
   test('Render del titulo', () => {
     expect(footer.find('.Footer-title').text()).toEqual('Platzi Store');
+  });
+});
+
+describe('Footer Snapshot', () => {
+  test('Comprobar la UI del componente Footer', () => {
+    const footer = create(<Footer />);
+    expect(footer.toJSON()).toMatchSnapshot();
   });
 });
--- src/__test__/components/Header.test.js
+++ src/__test__/components/Header.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { mount, shallow } from 'enzyme';
 import ProviderMock from '../../__mocks__/ProviderMock';
 import Header from '../../components/Header';
+import { create } from 'react-test-renderer';
 
 describe('<Header/>', () => {
   test('Render del componente Header', () => {
@@ -21,5 +22,16 @@ describe('<Header/>', () => {
       </ProviderMock>
     );
     expect(header.find('.Header-title').text()).toEqual('Platzi Store');
+  });
+});
+
+describe('Header Snapshot', () => {
+  test('Comprobar el snapshot de Header', () => {
+    const header = create(
+      <ProviderMock>
+        <Header />
+      </ProviderMock>
+    );
+    expect(header.toJSON()).toMatchSnapshot();
   });
 });
