dab9bf7f377f5475abb350ef11fc2b3ad0a760b0
Alberto González Balderas <albertoglezb@hotmail.com>
2019-12-28 11:27:25 -0600

    Se creo el mock ProviderMock.js. Se termino la clase: 'Crear mocks'.

--- src/__test__/components/Footer.test.js
+++ src/__test__/components/Footer.test.js
@@ -3,8 +3,11 @@ import { mount } from 'enzyme';
 import Footer from '../../components/Footer';
 
 describe('Test footer', () => {
+  const footer = mount(<Footer />);
   test('Render del componente footer', () => {
-    const footer = mount(<Footer />);
     expect(footer.length).toEqual(1);
+  });
+  test('Render del titulo', () => {
+    expect(footer.find('.Footer-title').text()).toEqual('Platzi Store');
   });
 });
